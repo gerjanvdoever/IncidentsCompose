8ead489c33e313d183e97547e40dd744adf6ead5
gerjanvdoever <gerjanvandenoever@outlook.com>
2025-11-03 16:52:45 +0100

    setup 2

--- app/src/main/java/com/example/incidentscompose/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/incidentscompose/navigation/AppNavigation.kt
@@ -9,9 +9,13 @@ import androidx.navigation3.ui.NavDisplay
 import com.example.incidentscompose.ui.screens.auth.LoginScreen
 import com.example.incidentscompose.ui.screens.auth.RegisterScreen
 import com.example.incidentscompose.ui.screens.auth.UserProfileScreen
+import com.example.incidentscompose.ui.screens.incidents.MyIncidentDetailScreen
 import com.example.incidentscompose.ui.screens.incidents.MyIncidentListScreen
 import com.example.incidentscompose.ui.screens.management.IncidentDetailScreen
-
+import com.example.incidentscompose.ui.screens.management.IncidentListScreen
+import com.example.incidentscompose.ui.screens.management.IncidentMapScreen
+import com.example.incidentscompose.ui.screens.management.UserManagementScreen
+import com.example.incidentscompose.ui.screens.incidents.ReportIncidentScreen
 
 @Composable
 fun AppNavigation() {
@@ -38,8 +42,43 @@ fun AppNavigation() {
 
             entry<RegisterKey> {
                 RegisterScreen(
-                    onNavigateToLogin = { backStack.removeLastOrNull() }
+                    onNavigateToLogin = {
+                        backStack.removeLastOrNull() }
                 )
+            }
+
+            entry<MyIncidentListKey> {
+                TODO("to be implemented")
+            }
+
+            entry<MyIncidentDetailKey> {
+                TODO("to be implemented")
+            }
+
+            entry<ReportIncidentKey> {
+                TODO("to be implemented")
+            }
+
+            entry<IncidentListKey> {
+                TODO("to be implemented")
+            }
+
+            entry<IncidentMapKey> {
+                TODO("to be implemented")
+            }
+
+            entry<UserManagementKey> {
+                TODO("to be implemented")
+            }
+
+            entry<UserProfileKey>{
+                TODO("to be implemented")
+                // data class UserProfileKey(val userJson: String) : NavKey
+            }
+
+            entry<IncidentDetailKey> {
+                TODO("to be implemented")
+                // data class IncidentDetailKey(val incidentId: Long) : NavKey
             }
         }
     )
